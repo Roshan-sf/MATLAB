78a79c820ee338f770c6291153a420c6becf6428
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2023-10-17 18:04:21 -0700

    HW 2 work

--- Aero215/HW 2 - Drag and Power/hw2_dragPower_Jaiswal_FerriRoshan.m
+++ Aero215/HW 2 - Drag and Power/hw2_dragPower_Jaiswal_FerriRoshan.m
@@ -1,18 +1,33 @@
 function [V, D, P] = hw2_dragPower_Jaiswal_FerriRoshan(v_max, aircraft)
 
 %Variables
-rho = .0023769; %rhoSealevel in slugs/ft^3
 R = 287.058; %Gas constant
 v_maxft = v_max*1.68781; %1.68781 is the conversion ratio for knots to ft/s
-weight = aircraft.WS*aircraft.S; %Calculating weight in lbf
+W = aircraft.WS*aircraft.S; %Calculating weight in lbf
 
-D = ((aircraft.CD0)*(aircraft.S)*(.5)*(rho)*(v_maxft))+((weight^2)/((.5)*(rho)*(v_maxft^2)*(pi)*(aircraft.AR)*(aircraft.S)*(aircraft.e)));
+%Breaking out structures to variabeles so equations are not so long
+WS = aircraft.WS; %in lbf/ft^2
+AR = aircraft.AR;
+CD0 = aircraft.CD0;
+e = aircraft.e;
+CLM = aircraft.CLM;
+S = aircraft.S; %ft^2
+rho = aircraft.rho;
 
 
 
+V_stall = sqrt((W)/((CLM)*(.5)*(rho)*(S))); %in ft/s
 
+%disp(['vstall:', num2str(V_stall)]) was used for debugging purposes
 
+V = linspace(V_stall,v_maxft,1000);
 
+    for i = 1:length(V)
+        D(i) = ((CD0)*(S)*(.5)*(rho)*(V(i)^2)+((W^2)/((.5)*(rho)*(V(i)^2)*(pi)*(AR)*(S)*(e)))); %in lbf
+        P(i) = ((D(i))*(V(i)))/(550); %in hp
+    end
 
-
+    
+V = V*0.592484;
+    
 end
