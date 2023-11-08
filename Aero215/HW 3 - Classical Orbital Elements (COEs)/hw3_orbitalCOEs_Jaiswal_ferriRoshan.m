80d24ab28705bca5bb0deb91a18f1e5c490ff73d
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2023-11-07 17:28:35 -0800

    hw3

--- Aero215/HW 3 - Classical Orbital Elements (COEs)/hw3_orbitalCOEs_Jaiswal_ferriRoshan.m
+++ Aero215/HW 3 - Classical Orbital Elements (COEs)/hw3_orbitalCOEs_Jaiswal_ferriRoshan.m
@@ -11,6 +11,7 @@ ui = [1,0,0];
 uj = [0,1,0];
 uk = [0,0,1];
 h = cross(R,V);
+h2 = dot(R,V);
 
 uiM = norm(ui); %the magnitudes of the values above
 ujM = norm(uj);
@@ -18,7 +19,7 @@ ukM = norm(uk);
 hM = norm(h);
 
 
-%% Part 2: Initial Calculations for lalter
+%% Part 2: Initial Calculations for later
 
 ep = ((VM^2)/2)-((mu)/RM); %Calculating Epsilon (specific mechanical energy) in J/kg
 
@@ -36,55 +37,37 @@ eV = (1/mu)*((((VM^2)-((mu)/(RM)))*R)-(dot(R,V)*V));
 
 e = norm(eV);
 
-%% Part 5:
+%% Part 5: inclination in rad
 
 i = acos((dot(uk,h))/((hM)*(ukM))); %in rad not deg
 
 
-%% Part 6: RAAN
+%% Part 6: RAAN in rad
 
-n = cross(uk,h); %projection of momentum vector in orbital plane
+n = cross(uk,h); %projection of momentum vector in orbital plane and node line?
 nM = norm(n);
 
-RAAN = acos((dot(ui,n))/((uiM)*(nM)));
+if n(2) >= 0
+    RAAN = acos((dot(ui,n))/((uiM)*(nM))); %original equation
+else
+    RAAN = (2*pi)-(acos((dot(ui,n))/((uiM)*(nM))));
+end
 
-%% Part 7: Argument of Periapsis
+%% Part 7: Argument of Periapsis in rad
 
-w = acos(dot(n,eV)/(nM*e));
+if eV(3) <= 0
+    w = (2*pi)-(acos(dot(n,eV)/(nM*e)));
+else
+    w = acos(dot(n,eV)/(nM*e));
+end
 
-%% Part 8: nu (or theta)
+%% Part 8: nu (or theta) true anomaly in rad
 
-nu = acos(dot(eV,R)/(e*RM));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if h2 >= 0
+    nu = acos(dot(eV,R)/(e*RM));
+else
+    nu = (2*pi)-(acos(dot(eV,R)/(e*RM)));
+end
 
 
 
