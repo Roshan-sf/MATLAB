2fd01532deba9b259945b1e9b243faea49d532f2
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2024-04-20 08:28:57 -0700

    Update RoshanJaiswalFerri_Lab3.m

--- Aero300/Lab 3 -  Iterative Methods to Solve Scalar Equations/RoshanJaiswalFerri_Lab3.m
+++ Aero300/Lab 3 -  Iterative Methods to Solve Scalar Equations/RoshanJaiswalFerri_Lab3.m
@@ -8,5 +8,59 @@ close all;      %Clears all
 clear all;      %Clears Workspace
 clc;            %Clears Command Window
 
-%% PART 1: Bracketing Method
+%% PART 1: Using Bracketing Method
+
+
+fx = @(x) x^3+1.0142*x^2-19.3629*x+15.8398;
+
+d = 3; %Degree of polynomial
+s = 1; %Step size
+g = -6; %inital guess
+
+
+%M = roots()
+
+%% Bracketing Function
+
+%rootMatrix = roots(initial_guess, step_size, expected_num_roots, function)
+
+for row = 1:d %two for loops creating a 3x4 matrix
+    for col = 1:2
+        M(row,col) = 0;       
+    end
+end
+
+lBracket = g;
+
+% for i = 1:d
+%     hBracket = lBracket+s;
+%     h = fx(hBracket);
+%     l = fx(lBracket);
+%     if h*l<0 %if it is <0 then ans is negative and there is root in bracket
+%         while done == 0 
+%             hBracket2 = lBracket + e-6;
+%             h = fx(hBracket2);
+%             l = fx(lBracket);
+%             if h*l<0
+% 
+% 
+%         end
+%     elseif h*l==0
+%         %code
+%     else
+%         %code
+%     end
+    
+
+
+
+
+
+%end
+
+
+
+
+
+
 
