b7f08cc8e765dae93ef90aa20c6ba87d91ee5f40
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2024-05-02 13:55:56 -0700

    Update RoshanJaiswalFerri_Lab4.m

--- Aero300/Lab 4 - Iterative Methods to Solve Matrix Equations/RoshanJaiswalFerri_Lab4.m
+++ Aero300/Lab 4 - Iterative Methods to Solve Matrix Equations/RoshanJaiswalFerri_Lab4.m
@@ -6,7 +6,7 @@
 
 format long     %Allows for more accurate decimals
 close all;      %Clears all
-clear all;      %Clears Workspace
+%clear all;      %Clears Workspace
 clc;            %Clears Command Window
 
 %% PART 1: 
@@ -16,11 +16,36 @@ a = [3,-1,2];
 b = [1,4,2];
 c = [-1,3,5];
 
-A = [a;b;c];
+%A = [a;b;c];
 B = [1;1;1];
-d = diag(A);
+%d = diag(A);
+y = randi([2 9]);
+z = randi([2 9]);
+A = randi([-10 10],y,z);
+s = size(A);
+isDom = 0; %Diagonal Dominance 0 is dominant 1 is not
 
-for 
-abs(A(1,1)) >= abs(A(1,2) + A(1,3))
+%% PART 2: Testing for Diagonal Dominance
 
+for i = 1:s(1,:) %For loop testing for diagonal dominance
+    x=0;
+    if j > s(1,2) || i > s(1,1) %stopping the loop if the matrix is mxn and not nxn and it is looping past an existing column or row
+        break
+    end
+    for j = 1:s(:,1)
+        x = x + A(i,j);
+    end
+    x = x - A(i,i);
+    if A(i,i) < x %if any of the rows are not dominant set isDom to not dom
+        isDom = 1; 
+    end
 end
+
+if isDom == 1
+    disp('Warning: May not Converge')
+elseif isDom == 0
+    disp('Matrix Will converge')
+else
+    disp('error (isDom)')
+end
+
