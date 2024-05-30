fcd8eab386e2290c6735e625fd0e80145c441d25
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2024-05-30 10:34:11 -0700

    stuff

---
+++ Aero300/Lab 8 - Beam Deflections and PDEs/RoshanJaiswalFerri_Lab8.m
@@ -0,0 +1,33 @@
+%Roshan Jaiswal-Ferri
+%Section - 03
+%Aero 300 Lab 8 - Beam Deflections and PDEs: 5/24/24
+
+%% Workspace Prep
+
+format long     %Allows for more accurate decimals
+close all;      %Clears all
+clear all;      %Clears Workspace
+clc;            %Clears Command Window
+
+%% PART 1: Using RKF45 to Solve an ODE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
