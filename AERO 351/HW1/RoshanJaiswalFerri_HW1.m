d44f9557a2e4495045e6f62791bc33bf8c4cb756
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2024-09-23 20:46:05 -0700

    A351

---
+++ AERO 351/HW1/RoshanJaiswalFerri_HW1.m
@@ -0,0 +1,21 @@
+%Roshan Jaiswal-Ferri
+%Section - 01
+%Aero 351 Homework 1 - Datetime Calcs: 9/23/24
+
+%% Workspace Prep
+
+format long     %Allows for more accurate decimals
+close all;      %Clears all
+clear all;      %Clears Workspace
+clc;            %Clears Command Window
+
+%% PART 1: Converting Mean Solar to JD time
+
+day = 23;
+month = 9;
+year = 2024;
+ut = 12:00:00;
+
+
+
+JDtime = tojd(day,month,year,ut);
