93cbabbe641c73eb4127d213e4f38f9bc7563276
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2024-10-10 11:16:17 -0700

    fluids

--- AERO 302 - Fluids/Lab 1 - Venturi Effect and Open Jet/Lab1.m
+++ AERO 302 - Fluids/Lab 1 - Venturi Effect and Open Jet/Lab1.m
@@ -18,22 +18,56 @@ clc;            %Clears Command Window
 %test section 1.0472 m^2\
 
 %Units 1 R = 0.8128 M, .25 R 
-A = readmatrix('D3.xlsx');
-len = length(A(1,:));
+% A = readmatrix('D3.xlsx');
+% len = length(A(1,:));
+% 
+% 
+% R = .8128;
+% dy = .25*R;
+% dx = .25*R;
+% 
+% for i = 1:length(A)
+%     for j = 1:height(A)
+% 
+%     end
+% end
 
-
-R = .8128;
-dy = .25*R;
-dx = .25*R;
-
-for i = 1:length(A)
-    for j = 1:height(A)
-
-    end
-end
+%Jet entrainment
 
 %% Surface Plot
 
+% Load data
+data = readmatrix('Data\D1.xlsx');
+data2 = (readmatrix('Data\D2.xlsx'))'; 
+data3 = readmatrix('Data\D3.xlsx'); 
+data4 = (readmatrix('Data\D4.xlsx'))'; 
+data5 = (readmatrix('Data\D5.xlsx'))'; 
+data6 = readmatrix('Data\D6.xlsx'); 
+
+%%
+
+
+% Generate grid (adjust grid spacing as needed)
+[X, Y] = meshgrid(1:size(data, 2), 1:size(data, 1));
+
+filename = [data, data2, data3, data4, data5, data6];
+
+% Create contour plot
+figure
+contour3(X, Y, data3);
+%surf(X, Y, filename);
+colorbar;
+title('Contour Plot for Data 1'); % Adjust title for each plot
+xlabel('X-axis');
+ylabel('Y-axis');
+
+figure
+contour3(X, Y, data4);
+%surf(X, Y, filename);
+colorbar;
+title('Contour Plot for Data 1'); % Adjust title for each plot
+xlabel('X-axis');
+ylabel('Y-axis');
 
 
 
