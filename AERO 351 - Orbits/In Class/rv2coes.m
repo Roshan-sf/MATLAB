ba3d96a3cd8227eb584668125434c6b0dae31f7c
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2024-10-16 15:18:36 -0700

    rv2coes

---
+++ AERO 351 - Orbits/In Class/rv2coes.m
@@ -0,0 +1,87 @@
+function [hM,a,e,nu,i,RAAN,w,p,t,energy] = rv2coes(R,V,mu)
+
+%ALL IN RADIAN!!!!!!!!
+
+RM = norm(R); %Magnitude of R
+VM = norm(V); %Magnitude of V
+
+ui = [1,0,0];
+uj = [0,1,0];
+uk = [0,0,1];
+h = cross(R,V);
+h2 = dot(R,V);
+
+uiM = norm(ui); %the magnitudes of the values above
+ujM = norm(uj);
+ukM = norm(uk);
+hM = norm(h); %Calculating specific energy
+
+
+%% PART 1: Initial Calculations for later
+
+ep = ((VM^2)/2)-((mu)/RM); %Calculating Epsilon (specific mechanical energy) in J/kg
+
+
+%% PART 2: Calculating semi-major axis
+
+a = -((mu)/(2*ep)); %in km
+
+%% PART 3: Genreal equation calculation for period
+
+p = (2*pi)*sqrt((a^3)/(mu)); %period of orbit in seconds
+
+%% PART 4: Calculating eccentricity
+eV = (1/mu)*((((VM^2)-((mu)/(RM)))*R)-(dot(R,V)*V)); %eccentricity vector is from origin to point of periapsis 
+
+e = norm(eV);
+
+%% PART 5: inclination in rad
+
+i = acos((dot(uk,h))/((hM)*(ukM))); %in rad not deg
+
+
+%% PART 6: RAAN in rad
+
+n = cross(uk,h); %projection of momentum vector in orbital plane and node line?
+nM = norm(n);
+
+if n(2) >= 0
+    RAAN = acos((dot(ui,n))/((uiM)*(nM))); %original equation
+else
+    RAAN = (2*pi)-(acos((dot(ui,n))/((uiM)*(nM))));
+end
+
+%% PART 7: Argument of Periapsis in rad
+
+if eV(3) >= 0 %k component of eccentricity vector (height)
+    w = acos(dot(n,eV)/(nM*e));
+else
+    w = (2*pi)-(acos(dot(n,eV)/(nM*e)));
+end
+
+%% PART 8: nu (or theta) true anomaly in rad
+
+if h2 >= 0 %dot product of R and V idk what it represents
+    nu = acos(dot(eV,R)/(e*RM));
+else
+    nu = (2*pi)-(acos(dot(eV,R)/(e*RM)));
+end
+
+%% PART 9: Time siince perigee passage
+
+E = 2*atan(sqrt((1-e)/(1+e))*tan(nu/2));
+Me = E - e*sin(E);
+n = (2*pi)/p;
+t = Me/n; %in seconds
+
+if t < 0 %If it is negative it is other way around circle think 360-angle
+    t = p + t; %this shows adding but it is adding a negative
+end
+
+%% PART 10: Calculating Energy
+
+energy = (VM^2)/2 - mu/RM;
+
+
+
+end
