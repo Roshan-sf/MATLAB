19db717a24b77b2e811d7d1dd1b39f118195f2e5
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2025-05-02 16:38:40 -0700

    Create AERO446_HW4_RJF.m

---
+++ AERO 446 - Electrical Systems/HW/AERO446_HW4_RJF.m
@@ -0,0 +1,13 @@
+%% Roshan Jaiswal-Ferri
+%Section - 01
+%Aero 446 HW3: 4/25/25
+
+%% Workspace Prep
+
+%warning off
+format long     %Allows for more accurate decimals
+close all;      %Clears all
+clear all;      %Clears Workspace
+clc;            %Clears Command Window
+
+%% 
