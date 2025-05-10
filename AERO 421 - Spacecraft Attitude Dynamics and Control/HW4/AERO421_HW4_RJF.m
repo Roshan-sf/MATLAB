c726e37a9f7367c077b122d100535cf3171309c4
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2025-05-09 19:22:10 -0700

    stuff

--- AERO 421 - Spacecraft Attitude Dynamics and Control/HW4/AERO421_HW4_RJF.m
+++ AERO 421 - Spacecraft Attitude Dynamics and Control/HW4/AERO421_HW4_RJF.m
@@ -12,3 +12,19 @@ clc;            %Clears Command Window
 
 %% Problem 1
 
+JD = 2458981.1666667;
+
+r0 = [5980.8297; -1282.3184; 4125.8019];  % km in ECI
+v0 = [1.540887; 7.186813; 0];  % km/s
+
+target_lat = 35.3;       % degrees
+target_lon = -120.8;     % degrees
+target_alt = 0.2;        % km
+
+target_eci = lla2eci(target_lat, target_lon, target_alt, JD);
+
+%% Functions
+
+function [g, g_norm, T_g] = geo_monte_sim(r_0, v_0, p_hat_0, T_0, jd, errors, n)
+
+end
