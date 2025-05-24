97fe37c45e80a8bf15d6f8c89687fffe2a1836e9
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2025-05-23 17:34:00 -0700

    ewr

---
+++ AERO 421 - Spacecraft Attitude Dynamics and Control/HW5/Final_Project_Part_2_init_Template.m
@@ -0,0 +1,66 @@
+%% Final Simulation Project
+% Part 2 Template
+% Aero 421
+% Eric Mehiel
+% Cal Poly, SLO
+
+clear
+close all
+clc
+
+%% Part 1 - Mass Properties
+
+% Compute Mass properties for normal operations phase
+
+
+%% Part 2 - Torque Free Attitudue Simulation
+
+% Spacecraft Orbit Properties (given)
+global mu
+mu = 398600; % km^3/s^2
+h = 53335.2; % km^2/s
+e = 0; % none
+Omega = 0*pi/180; % radians
+inclination = 98.43*pi/180; % radians
+omega = 0*pi/180; % radians
+nu = 0*pi/180; % radians
+
+a = h^2/mu/(1 - e^2);
+orbital_period = 2*pi*sqrt(a^3/mu);
+
+% Torque free scenario (Given)
+T = [0;0;0];
+
+% Set/Compute initial conditions
+% intial orbital position and velocity
+[r_ECI_0, v_ECI_0] = coe2rv([h e Omega inclination omega nu]);
+
+% Compute inital F_LVLH basis vectors in F_ECI components based on F_LVLH
+% definition
+
+
+% Initial Euler angles relating F_body and F_LVLH (given)
+phi_0 = 0;
+theta_0 = 0;
+psi_0 = 0;
+E_b_LVLH_0 = [phi_0; theta_0; psi_0];
+
+% Initial Quaternion relating F_body and F_LVLH (given)
+q_b_LVLH_0 = [0; 0; 0; 1];
+
+% Compute initial C_LVLH_ECI_0, C_b_LHVL_0, and C_b_ECI_0 rotaiton matrices
+
+% Initial Euler angles relating body to ECI
+E_b_ECI_0 = C2EulerAngles(C_b_ECI_0);
+
+% Initial quaternion relating body to E
+q_b_ECI_0 = C2quat(C_b_ECI_0);
+
+% Initial body rates of spacecraft (given)
+w_b_ECI_0 = [0.001; -0.001; 0.002];
+
+tspan = orbital_period;
+
+out = sim('Final_Project_Solutions_Part_2');
+
+%% Plot Results
