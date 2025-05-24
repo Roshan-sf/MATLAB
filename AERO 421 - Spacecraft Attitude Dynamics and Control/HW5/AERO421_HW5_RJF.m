bb65267cb0f2155ee468f757dbd35164f0f07409
Roshan <43590339+Roshan-sf@users.noreply.github.com>
2025-05-23 17:59:48 -0700

    efw

--- AERO 421 - Spacecraft Attitude Dynamics and Control/HW5/AERO421_HW5_RJF.m
+++ AERO 421 - Spacecraft Attitude Dynamics and Control/HW5/AERO421_HW5_RJF.m
@@ -10,5 +10,29 @@ close all;      %Clears all
 clear all;      %Clears Workspace
 clc;            %Clears Command 
 
-%% Question 1
+%% Initial Conditions
 
+J = diag([1200,2000,2800]);
+
+% Initial condition epsilon_0
+epsilon_0 = [0.2; -0.5; 0.3];
+
+% Compute eta_0
+eta_0 = sqrt(1 - epsilon_0' * epsilon_0);
+
+% Initial angular velocity vector omega_0
+omega_0 = [0.1; -0.05; 0.05]; % rad/s
+
+% Case 1: Initial conditions
+epsilon_c1 = [0; 0; 0];
+eta_c1 = 1;
+
+% Case 2: Initial conditions
+epsilon_c2 = [-0.2; 0.4; 0.2];
+eta_c2 = sqrt(1 - epsilon_c2' * epsilon_c2);
+
+tspan = 140;
+
+%% Simulink
+
+out = sim("ADCS_Design_RJF.slx");
